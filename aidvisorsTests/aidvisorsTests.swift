83af3ed5cb4c07e3241311348853c96e33667fcc
Rory Geddes <rorygeddes16@gmail.com>
2025-03-21 16:29:32 -0300

    Initial Commit

---
+++ aidvisorsTests/aidvisorsTests.swift
@@ -0,0 +1,16 @@
+//
+//  aidvisorsTests.swift
+//  aidvisorsTests
+//
+//  Created by Rory Geddes on 2025-03-21.
+//
+
+import Testing
+
+struct aidvisorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
